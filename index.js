67236d1fb175cf5f19fc6182348c482c4c05952e
chronize <sort@programmer.net>
2014-09-23 21:59:44 -0400

    initial stable

---
+++ index.js
@@ -0,0 +1,66 @@
+var fs = require('fs'),
+	path = require('path'),
+	async = require('async'),
+	jade = require('jade');
+
+module.exports = function(source, destination) {
+	return function(req, res, next) {
+        async.parallel({
+            templates: function(next) {
+                fs.readdir(source, next);
+            },
+            compiled: function(next) {
+                fs.readdir(destination, next);
+            }
+        }, function(err, data) {
+            async.each(data.templates, function(tpl, next) {
+                if (path.extname(tpl) === '.jade') {
+                    var newfile = path.basename(tpl, '.jade') + '.js';
+                    if (data.compiled.indexOf(newfile) > -1) {
+                        async.parallel({
+                            source: function(next) {
+                                fs.stat(path.join(source, tpl), next);
+                            },
+                            destination: function(next) {
+                                fs.stat(path.join(destination, newfile), next);
+                            }
+                        }, function(err, data) {
+                            if (data.source.mtime.getTime() != data.destination.mtime.getTime()) {
+                                async.waterfall([
+                                    function(next) {
+                                        fs.readFile(path.join(source, tpl), next);
+                                    },
+                                    function(data, next) {
+                                        fs.writeFile(path.join(destination, newfile), jade.compileClient(data), next);
+                                    },
+                                    function(next) {
+                                        fs.utimes(path.join(source, tpl), new Date, new Date(), next);
+                                    }
+                                ], next);
+                            }
+                            else {
+                                next();
+                            }
+                        });
+                    }
+                    else {
+                        async.waterfall([
+                            function(next) {
+                                fs.readFile(path.join(source, tpl), next);
+                            },
+                            function(data, next) {
+                                fs.writeFile(path.join(destination, newfile), jade.compileClient(data), next);
+                            },
+                            function(next) {
+                                fs.utimes(path.join(source, tpl), new Date, new Date(), next);
+                            }
+                        ], next);
+                    }
+                }
+                else {
+                    next();
+                }
+            }, next);
+        });
+    })
+}
